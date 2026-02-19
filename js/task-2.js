2e3e20ce8e843f21475258042286c83dabce64ed
DP <dpkds161@gmail.com>
2026-02-19 10:57:27 +0200

    done task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,14 @@
 function getShippingMessage(country, price, deliveryFee) {
   // rishennya
+  const toPay = price + deliveryFee;
+  console.log(`Shipping to ${country} will cost ${toPay} credits`);
+  return toPay;
 }
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+getShippingMessage('Australia', 120, 50);
+getShippingMessage('Germany', 80, 20);
+getShippingMessage('Sweden', 100, 20);
+
+console.log(getShippingMessage('Australia', 120, 50)); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingMessage('Germany', 80, 20)); // "Shipping to Germany will cost 100 credits"
+console.log(getShippingMessage('Sweden', 100, 20)); // "Shipping to Sweden will cost 120 credits"
